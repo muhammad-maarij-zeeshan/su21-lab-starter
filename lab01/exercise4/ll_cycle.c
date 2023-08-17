cc8d2c77a2ccd15d6e8c293ea2ee4fbdf14d6404
Muhammad Maarij Zeeshan <muhammad.maarij@10xengineers.ai>
2023-08-17 14:42:36 +0500

    lab01 exercise4

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,19 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    node *fast_ptr = head, *slow_ptr = head;
+    if (head == NULL) {
+        return 0;
+    }
+    while (fast_ptr->next) {
+        fast_ptr = fast_ptr->next;
+        if (fast_ptr->next) {
+            fast_ptr = fast_ptr->next;
+        }
+        slow_ptr = slow_ptr -> next;
+        if (slow_ptr == fast_ptr) {
+            return 1;
+        }
+    }
+    return 0;
 }
