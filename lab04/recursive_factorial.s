8da0dd3622eec0af5f69a4fec3bd66c89d6931f8
muhammad-maarij-zeeshan <142016896+muhammad-maarij-zeeshan@users.noreply.github.com>
2023-10-24 12:41:15 +0500

    Recursive Factorial Programming Task

--- lab04/recursive_factorial.s
+++ lab04/recursive_factorial.s
@@ -3,46 +3,39 @@
 .globl main
 
 main:
-    # TO TERMINATE SPIKE #
-    la      t1, tohost
-    li      t2, 1
-    sw      t2, 0(t1)
-    
     la a0, num     # load address of num into a0
+    lw a1, 0(a0)
+    mv a0, a1
     jal factorial  # call factorial function
-    la a1, ans  # load address of result into a1
-    sw a0, 0(a1)   # store result into result variable
-    ecall
+    la a0, ans     # load address of result into a1
+    sw a1, 0(a0)   # store result into result variable
     j end          # jump to end of program
 
 factorial:
     # PROLOGUE START #
     addi sp, sp, -8
-    sw ra, 4(sp)
-    sw a0, 0(sp)
+    sw ra, 0(sp)
+    sw a1, 4(sp)
     # PROLOGUE END #
-
-    lw a1, 0(a0)
 
     # if (num <= 1) return 1
     li t0, 1
     ble a1, t0, base_case
 
-    addi a1, a1, -1       # num = num -1
+    addi a1, a1, -1      # num = num -1
     jal factorial        # factorial (num -1)
-    lw a1, 0(sp)         # Original num
-    mul a0, a1, a0       # num * factorial(num - 1)
+    lw t1, 4(sp)         # Original num
+    mul a1, a1, t1       # num * factorial(num - 1)
 
     base_case:
-        # EPILOGUE START #
-        lw a0, 0(sp)
-        lw ra, 4(sp)
+        # EPILOGUE START 
+        lw ra, 0(sp)
         addi sp, sp, 8
         # EPILOGUE END #
         ret
 
 end:
-# end code here
+    # end code here
 
 write_tohost:
 li x1, 1
@@ -51,7 +44,7 @@ j write_tohost
 
 .data 
 # start data section here
-num: .word 5
+num: .word 4
 ans: .word 0
 # end data section here
 
